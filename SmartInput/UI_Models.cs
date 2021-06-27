3ee7bb2645e29cc7c60eb157bd166837e601bb1c
NEATShonen <girishagodesuotakusan@gmail.com>
2021-06-27 11:58:56 -0700

    Create UI_Models.cs

---
+++ SmartInput/UI_Models.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class SmartInput_ValidationTypes
+    {
+        public static int Type_UInteger = 0;
+        public static int Type_UDecimal = 1;
+        public static int Type_Integer = 2;
+        public static int Type_Decimal = 3;
+        public static int Type_String = 4;
+        public static int Type_PostalCode = 6;
+        public static int Type_MobileNumber = 7;
+        public static int Type_TIN = 8;
+        public static int Type_Year = 9;
+        public static int Type_Month = 10;
+        public static int Type_Day = 11;
+        public static int Type_Hour = 12;
+        public static int Type_Minute = 13;
+        public static int Type_Second = 14;
+        public static int Type_inList = 14;
+
+    }
+}
