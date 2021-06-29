71caae9bf83749640214b8c5d0b89a594fd835a4
NEATShonen <girishagodesuotakusan@gmail.com>
2021-06-29 05:56:21 -0700

    Update UI_Models.cs

--- SmartInput/UI_Models.cs
+++ SmartInput/UI_Models.cs
@@ -15,14 +15,16 @@ namespace Shared.Models
         public static int Type_String = 4;
         public static int Type_PostalCode = 6;
         public static int Type_MobileNumber = 7;
-        public static int Type_TIN = 8;
+        public static int Type_AFM = 8;
         public static int Type_Year = 9;
         public static int Type_Month = 10;
         public static int Type_Day = 11;
         public static int Type_Hour = 12;
         public static int Type_Minute = 13;
         public static int Type_Second = 14;
-        public static int Type_inList = 14;
+        public static int Type_inList = 15;
+        public static int Type_Email = 16;
+        public static int Type_Password = 17;
 
     }
 }
