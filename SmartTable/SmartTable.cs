6bdbd09fef0f24c57be2ec0bfbd5dd90271b0417
NEATShonen <girishagodesuotakusan@gmail.com>
2021-07-07 19:31:52 +0300

    Create SmartTable.cs

---
+++ SmartTable/SmartTable.cs
@@ -0,0 +1,8 @@
+ public class SmartTableFilters
+    {
+        public FieldInfo field;
+        public string sorting = "";
+        public string filter= "";
+        public string filtertype = "";
+
+    }
